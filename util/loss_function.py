ccbc469f3f961c50474d9ca143c1ea65106e5a3e
ilcessadecalcular <1185673942@qq.com>
2022-08-07 15:40:01 +0800

    first

--- util/loss_function.py
+++ util/loss_function.py
@@ -243,3 +243,33 @@ class BCEFocalLoss(torch.nn.Module):
         elif self.reduction == 'sum':
             loss = torch.sum(loss)
         return loss
+
+
+class SoftDiceLoss(nn.Module):
+    def __init__(self, weight=None, size_average=True):
+        super(SoftDiceLoss, self).__init__()
+
+    def forward(self, logits, targets):
+        num = targets.size(0)
+        smooth = 1
+
+        probs = F.sigmoid(logits)
+        m1 = probs.view(num, -1)
+        m2 = targets.view(num, -1)
+        intersection = (m1 * m2)
+
+        score = 2. * (intersection.sum(1) + smooth) / (m1.sum(1) + m2.sum(1) + smooth)
+        score = 1 - score.sum() / num
+        return score
+
+
+class BCELoss2d(nn.Module):
+    def __init__(self, weight=None, size_average=True):
+        super(BCELoss2d, self).__init__()
+        self.bce_loss = nn.BCELoss(weight, size_average)
+
+    def forward(self, logits, targets):
+        probs = F.sigmoid(logits)  # 二分类问题，sigmoid等价于softmax
+        probs_flat = probs.view(-1)
+        targets_flat = targets.view(-1)
+        return self.bce_loss(probs_flat, targets_flat)
